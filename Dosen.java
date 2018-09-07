aa7bc8bd048867ca50f36a17b806ce81b20236ab
mahenbal <muhammad.iqbal610@ui.ac.id>
2018-09-07 16:29:12 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,28 @@
+public class Dosen{
+	private String pekerjaan;
+	private String jabatan;
+	private String umur;
+	private String nama;
+	private String gender;
+	
+	public Dosen(String pekerjaan, String jabatan, String nama, String umur, String gender) {
+		this.pekerjaan = pekerjaan;
+		this.nama = nama;
+		this.gender = gender;
+		this.umur = umur;
+		this.jabatan = jabatan;
+	}
+
+	public String getDosen() {
+		return nama;
+	}
+
+	public void setDosen(String nama) {
+		this.nama = nama;
+	}
+	
+	public void changeName(String updatedNama) {
+		this.nama = updatedNama;
+	}
+
+}
